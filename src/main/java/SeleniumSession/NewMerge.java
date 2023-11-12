948d5a267204491f33addd3155cf0b564791f5bd
Jain <samarjain@deloitte.com>
2023-11-12 15:06:05 +0530

    Changes NewMerge File

--- src/main/java/SeleniumSession/NewMerge.java
+++ src/main/java/SeleniumSession/NewMerge.java
@@ -4,13 +4,18 @@ public class NewMerge {
 
 	public static void main(String[] args) {
 
-		System.out.println("remote -- merge");
+		System.out.println("local -- merge");
 
 	}
-
+	
 	public void login()
 	{
-		System.out.println("remote -- login");
+		System.out.println("local -- login");
+	}
+	
+	public void m1()
+	{
+		System.out.println("M1 Method..");
 	}
 
 }
